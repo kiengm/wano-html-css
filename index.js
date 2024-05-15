2cfe4c417058194b73071f5e1142e70d1548a6ab
kiennd-wano <kiennd@wanosoft.com>
2024-05-15 17:46:32 +0700

    fix page 1

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// let isDisplay = false;
-// const element = document.getElementById("noti");
-// console.log(element);
-// // element.onclick = function () {
-// //   console.log(22222222222);
-// // };
 function loadNotification() {
-  console.log("Notification icon clicked!");
-  // Thực hiện các hành động khác khi thẻ li được nhấn
+  let isDisplay = false;
+  alert("xin chao");
+}
+
+let a = false;
+function changeLogo() {
+  const image = document.getElementById("id-image");
+
+  if (image.style.border) {
+    document.getElementById("id-image").style.removeProperty("border");
+    document.getElementById("id-image").style.removeProperty("borderRadius");
+    document.getElementById("id-image").src = "./public/asset/Ellipse 93.png";
+  } else {
+    document.getElementById("id-image").style.border = "2px solid blue";
+    document.getElementById("id-image").style.borderRadius = "50%";
+    document.getElementById("id-image").src = "./public/asset/Group 3616.png";
+  }
 }
