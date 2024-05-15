be621619fce5f17b666d5744885e9e69b5d481ab
kiennd-wano <kiennd@wanosoft.com>
2024-05-15 14:49:53 +0700

    page 2

---
+++ index.js
@@ -0,0 +1,10 @@
+// let isDisplay = false;
+// const element = document.getElementById("noti");
+// console.log(element);
+// // element.onclick = function () {
+// //   console.log(22222222222);
+// // };
+function loadNotification() {
+  console.log("Notification icon clicked!");
+  // Thực hiện các hành động khác khi thẻ li được nhấn
+}
